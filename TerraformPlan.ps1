a936b70a12d907e7bcf21b36369f7f9d59107550
Dwipractice <gaikwad.dwijal.18ee1082@gmail.com>
2025-04-15 17:34:02 +0530

    files modified

--- TerraformPlan.ps1
+++ TerraformPlan.ps1
@@ -1,11 +1,10 @@
 param(
     [Parameter(Mandatory = $True)] [string] $Environment,
-	[Parameter(Mandatory = $True)] [string] $ServicePrincipalSecret,
-	[Parameter(Mandatory = $True)] [string] $SharedSubscriptionId,
-	[Parameter(Mandatory = $True)] [string] $SharedTenantId,
-	[Parameter(Mandatory = $True)] [string] $SharedClientId,
-	[Parameter(Mandatory = $True)] [string] $SharedClientSecret,
-	[Parameter(Mandatory = $True)] [string] $SharedResourceGroup
+	[Parameter(Mandatory = $True)] [string] $SubscriptionId,
+	[Parameter(Mandatory = $True)] [string] $TenantId,
+	[Parameter(Mandatory = $True)] [string] $ClientId,
+	[Parameter(Mandatory = $True)] [string] $ClientSecret,
+	[Parameter(Mandatory = $True)] [string] $ResourceGroup
 )
 
 switch ($Environment) {
@@ -19,14 +18,8 @@ switch ($Environment) {
 
 Write-Host "`n> Applying changes ..." -ForegroundColor Cyan
 
-Set-Location $PSScriptRoot
 
-$azContext = Get-AzContext
 
-$env:ARM_CLIENT_ID = $azContext.Account.Id
-$env:ARM_CLIENT_SECRET= $ServicePrincipalSecret
-$env:ARM_SUBSCRIPTION_ID = $azContext.Subscription.Id
-$env:ARM_TENANT_ID = $azContext.Tenant.Id
 
 $tfstateFile = './envs/' + $envName + '-tfstate-backend.conf'
 $varFile = './envs/' + $envName + '.tfvars'
@@ -37,8 +30,8 @@ terraform init --backend-config $tfstateFile -reconfigure
 terraform validate
 
 terraform plan -out="$plan" -var-file="$varFile" `
-	-var "shared_subscription_id=$SharedSubscriptionId" `
-	-var "shared_tenant_id=$SharedTenantId" `
-	-var "shared_client_id=$SharedClientId" `
-	-var "shared_client_secret=$SharedClientSecret" `
-	-var "shared_resource_group=$SharedResourceGroup"
+	-var "shared_subscription_id=$SubscriptionId" `
+	-var "shared_tenant_id=$TenantId" `
+	-var "shared_client_id=$ClientId" `
+	-var "shared_client_secret=$ClientSecret" `
+	-var "shared_resource_group=$ResourceGroup"
